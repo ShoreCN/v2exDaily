efc54ce1a394c982d3db4f3176a4956ec092901e
ShoreCN <cs06@163.com>
2017-09-28 23:22:00 +0800

    Add daily process
    Add the daily process by parsing the mission url.

--- v2exDaily.py
+++ v2exDaily.py
@@ -19,6 +19,7 @@ class v2ex(object):
         'Referer' : url,
     }
 
+    #未避免重复登录，初始化时创建session用于整个程序的使用
     def __init__(self, user, password):
         self.user = user
         self.password = password
@@ -34,9 +35,6 @@ class v2ex(object):
         user_element = bs.find('input', {'class' : 'sl', 'type':'text'})['name']
         password_element = bs.find('input', {'type' : 'password'})['name']
         once_element = bs.find('input', {'name' : 'once'})['value']
-        print('user[%s]'%user_element)
-        print('password[%s]'%password_element)
-        print('once[%s]'%once_element)
 
         #构造表单数据并进行登陆
         form_data = {
@@ -57,12 +55,24 @@ class v2ex(object):
             return True
 
     #进行每日签到任务获取金币
-    def daily():
+    def daily(self):
+        #打印每日签到任务标题
         daily_url = "https://www.v2ex.com/mission/daily"
+        daily_content = self.session.get(daily_url, headers = self.headers)
+        daily_bs = BeautifulSoup(daily_content.content, 'lxml')
+        print('%s' % daily_bs.find('h1').text)
+
+        #因为签到按钮链接存在变量，此处根据获取到的实际元素拼接签到任务的URL,并根据最后的页面显示判断是否成功签到
+        mission_url = "https://www.v2ex.com" + daily_bs.find('input', {'class':'super normal button'})['onclick'].split("'")[1]
+        res_daily = self.session.get(mission_url, headers = self.headers)
+        if BeautifulSoup(res_daily, 'lxml').find('li', {'class':'fa fa-ok-sign'}) is None:
+            print('Daily mission failed.')
+        else:
+            print('Daily mission successful.')
 
     #获取账户余额情况
-    def balance():
-        
+    def balance(self):
+        pass
 
 if __name__ == '__main__':
     user = input('Please input the user name:')
